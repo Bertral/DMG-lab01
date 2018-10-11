dfa7026928e4ecd7df1ff733cc2a12bf42c60917
Bertral <antoine.friant@gmail.com>
2018-10-11 19:50:57 +0200

    impl, cleaning, still work to do

--- src/main/java/ch/heigvd/iict/dmg/labo1/Main.java
+++ src/main/java/ch/heigvd/iict/dmg/labo1/Main.java
@@ -5,64 +5,79 @@ import ch.heigvd.iict.dmg.labo1.parsers.CACMParser;
 import ch.heigvd.iict.dmg.labo1.queries.QueriesPerformer;
 import ch.heigvd.iict.dmg.labo1.similarities.MySimilarity;
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.KeywordAnalyzer;
+import org.apache.lucene.analysis.core.StopAnalyzer;
+import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.search.similarities.Similarity;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// 1.1. create an analyzer
-		Analyzer analyser = getAnalyzer();
+        // 1.1. create an analyzer
+        Analyzer analyser = getAnalyzer();
 
-		// TODO student "Tuning the Lucene Score"
+        // TODO student "Tuning the Lucene Score"
 //		Similarity similarity = null;//new MySimilarity();
-		Similarity similarity = new MySimilarity();
-		
-		CACMIndexer indexer = new CACMIndexer(analyser, similarity);
-		indexer.openIndex();
-		CACMParser parser = new CACMParser("documents/cacm.txt", indexer);
-		parser.startParsing();
-		indexer.finalizeIndex();
-		
-		QueriesPerformer queriesPerformer = new QueriesPerformer(analyser, similarity);
+        Similarity similarity = new MySimilarity();
 
-		// Section "Reading Index"
-		readingIndex(queriesPerformer);
+        CACMIndexer indexer = new CACMIndexer(analyser, similarity);
+        indexer.openIndex();
+        CACMParser parser = new CACMParser("documents/cacm.txt", indexer);
+        parser.startParsing();
+        indexer.finalizeIndex();
 
-		// Section "Searching"
-		searching(queriesPerformer);
+        QueriesPerformer queriesPerformer = new QueriesPerformer(analyser, similarity);
 
-		queriesPerformer.close();
-		
-	}
+        // Section "Reading Index"
+        readingIndex(queriesPerformer);
 
-	private static void readingIndex(QueriesPerformer queriesPerformer) {
-		queriesPerformer.printTopRankingTerms("authors", 10);
-		queriesPerformer.printTopRankingTerms("title", 10);
-	}
+        // Section "Searching"
+        searching(queriesPerformer);
 
-	private static void searching(QueriesPerformer queriesPerformer) {
-		// Example
-		queriesPerformer.query("compiler program");
+        queriesPerformer.close();
 
-		// TODO student
+    }
+
+    private static void readingIndex(QueriesPerformer queriesPerformer) {
+        queriesPerformer.printTopRankingTerms("authors", 10);
+        queriesPerformer.printTopRankingTerms("title", 10);
+    }
+
+    private static void searching(QueriesPerformer queriesPerformer) {
+        // Example
+        queriesPerformer.query("compiler program");
+
+        // TODO student
         // queriesPerformer.query(<containing the term Information Retrieval>);
-		// queriesPerformer.query(<containing both Information and Retrieval>);
+        // queriesPerformer.query(<containing both Information and Retrieval>);
         // and so on for all the queries asked on the instructions...
         //
-		// Reminder: it must print the total number of results and
-		// the top 10 results.
-	}
+        // Reminder: it must print the total number of results and
+        // the top 10 results.
+    }
 
-	private static Analyzer getAnalyzer() {
-	    // TODO student... For the part "Indexing and Searching CACM collection
-		// - Indexing" use, as indicated in the instructions,
-		// the StandardAnalyzer class.
-		//
-		// For the next part "Using different Analyzers" modify this method
-		// and return the appropriate Analyzers asked.
+    private static Analyzer getAnalyzer() {
+        // TODO student... For the part "Indexing and Searching CACM collection
+        // - Indexing" use, as indicated in the instructions,
+        // the StandardAnalyzer class.
+        //
+        // For the next part "Using different Analyzers" modify this method
+        // and return the appropriate Analyzers asked.
 
-		return null; // TODO student
-	}
+        Map<String, Analyzer> analyzerPerField = new HashMap<>();
+        analyzerPerField.put("authors", new KeywordAnalyzer());
+        analyzerPerField.put("title", new StandardAnalyzer());
+        analyzerPerField.put("summary", new StopAnalyzer());
+        PerFieldAnalyzerWrapper analyzer = new PerFieldAnalyzerWrapper(
+                new StandardAnalyzer(), analyzerPerField);
+
+
+        return analyzer;
+    }
 
 }
--- src/main/java/ch/heigvd/iict/dmg/labo1/queries/QueriesPerformer.java
+++ src/main/java/ch/heigvd/iict/dmg/labo1/queries/QueriesPerformer.java
@@ -1,9 +1,18 @@
 package ch.heigvd.iict.dmg.labo1.queries;
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.FieldInfo;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.misc.HighFreqTerms;
+import org.apache.lucene.misc.TermStats;
+import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.similarities.Similarity;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
@@ -11,45 +20,81 @@ import org.apache.lucene.store.FSDirectory;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.List;
 
 public class QueriesPerformer {
-	
-	private Analyzer		analyzer		= null;
-	private IndexReader 	indexReader 	= null;
-	private IndexSearcher 	indexSearcher 	= null;
 
-	public QueriesPerformer(Analyzer analyzer, Similarity similarity) {
-		this.analyzer = analyzer;
-		Path path = FileSystems.getDefault().getPath("index");
-		Directory dir;
-		try {
-			dir = FSDirectory.open(path);
-			this.indexReader = DirectoryReader.open(dir);
-			this.indexSearcher = new IndexSearcher(indexReader);
-			if(similarity != null)
-				this.indexSearcher.setSimilarity(similarity);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    private Analyzer analyzer = null;
+    private IndexReader indexReader = null;
+    private IndexSearcher indexSearcher = null;
 
-	public void printTopRankingTerms(String field, int numTerms) {
-		// TODO student
-		// This methods print the top ranking term for a field.
-		// See "Reading Index".
-	    System.out.println("Top ranking terms for field ["  + field +"] are: ");
-	}
-	
-	public void query(String q) {
-		// TODO student
-		// See "Searching" section
+    public QueriesPerformer(Analyzer analyzer, Similarity similarity) {
+        this.analyzer = analyzer;
+        Path path = FileSystems.getDefault().getPath("index");
+        Directory dir;
+        try {
+            dir = FSDirectory.open(path);
+            this.indexReader = DirectoryReader.open(dir);
+            this.indexSearcher = new IndexSearcher(indexReader);
+            if (similarity != null)
+                this.indexSearcher.setSimilarity(similarity);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-		System.out.println("Searching for [" + q +"]");
-	}
-	 
-	public void close() {
-		if(this.indexReader != null)
-			try { this.indexReader.close(); } catch(IOException e) { /* BEST EFFORT */ }
-	}
-	
+    public void printTopRankingTerms(String field, int numTerms) {
+        // This methods print the top ranking term for a field.
+        try {
+
+            TermStats[] foundTerms = HighFreqTerms.getHighFreqTerms(indexReader, numTerms, field,
+                    new HighFreqTerms.TotalTermFreqComparator());
+
+            System.out.println("\nMost frequent terms in " + field + " :");
+
+            for (TermStats term : foundTerms) {
+                System.out.println(term.totalTermFreq + " : " + term.termtext.utf8ToString());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void query(String q) {
+        final int LIMIT = 20;
+
+        QueryParser parser = new QueryParser("summary", analyzer);
+
+        try {
+            Query query = parser.parse(q);
+            IndexSearcher searcher = new IndexSearcher(indexReader);
+            searcher.search(query, 100);
+
+            TopDocs results = searcher.search(query, LIMIT);
+            ScoreDoc[] hits = results.scoreDocs;
+
+            System.out.println("\nSearching for: " + q + " (" + results.totalHits + " results)");
+
+            for (int i = 0; i < LIMIT && i < results.totalHits; ++i) {
+                Document doc = searcher.doc(hits[i].doc);
+                System.out.println(doc.get("id") + ": " + doc.get("title"));
+            }
+
+            System.out.println();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\n");
+    }
+
+    public void close() {
+        if (this.indexReader != null)
+            try {
+                this.indexReader.close();
+            } catch (IOException e) { /* BEST EFFORT */ }
+    }
+
 }
